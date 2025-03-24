cf79488a2294d2069319790c8ad8fb4075fc775e
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-03-24 22:45:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function toCase(text) {
-  // write your code here
+  if (text === "") {
+    return "-";
+  }
+  return text.toLowerCase() + "-" + text.toUpperCase();
 }
 
 // DO not change the code below
-
-
 const text = prompt("Enter text:");
 alert(toCase(text));
